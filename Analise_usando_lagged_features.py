8a1f28875ff81714f2d7a4ca9b830049cbe436d1
Alef2021 <2021alef@gmail.com>
2025-07-20 23:24:05 -0300

    Update Analise_usando_lagged_features.py
    Atulização de graficos

--- Analise_usando_lagged_features.py
+++ Analise_usando_lagged_features.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -71,13 +73,21 @@ y_test = test['Target'].astype(int)
 
 model = LogisticRegression()
 
+
+#Adicionando StandardScale
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
 #trenar o modelo 
 
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
 #Efetuando previsao
 
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 #testes com valores de dias que não existem na base
 #y_pred = model.predict([[139.586,140.049,138.384,138.855,139.695,138.855]]) #02/07/2025
@@ -85,8 +95,59 @@ y_pred = model.predict(X_test)
 #y_pred = model.predict([[140.928,141.564,140.597,139.051,141.304,139.051]]) #04/07/2025
 #y_pred = model.predict([[141.265,141.342,139.295,140.928,141.564,140.597]]) #05/07/2025
 
+#y_pred = model.predict([[135.298,136.022,134.380,136.187,136.187,134.840]]) #15/07/2025
 
+#print(y_pred)
 
 
 acc = accuracy_score(y_test, y_pred)
 print(f"Acurácia: {acc:.2f} ({acc*100:.1f}%)")
+
+
+
+print("Relatório de Classificação:")
+print(classification_report(y_test, y_pred, target_names=["Queda (0)", "Alta (1)"]))
+
+
+# Calcula a matriz de confusão novamente (só por garantia)
+cm = confusion_matrix(y_test, y_pred)
+
+#matriz de confusão
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d')
+plt.title('Matriz de Confusão')
+plt.xlabel('Previsto')
+plt.ylabel('Real')
+plt.show()
+
+#########################################################################################################
+
+# # Evolução Histórica da base
+
+# plt.figure(figsize=(14, 6))
+# plt.plot(df_ibovespa['Data'], df_ibovespa['Último'], label='Fechamento (Último)')
+# plt.title('Evolução Histórica do Fechamento do IBOVESPA')
+# plt.xlabel('Data')
+# plt.ylabel('Índice')
+# plt.grid(True)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
+
+# #Distribuição da Variação Diária (% Var)
+# plt.figure(figsize=(8, 4))
+# sns.histplot(df_ibovespa['Var%'], bins=50, kde=True, color='skyblue')
+# plt.title('Distribuição das Variações Diárias do IBOVESPA')
+# plt.xlabel('Variação Percentual Diária')
+# plt.ylabel('Frequência')
+# plt.tight_layout()
+# plt.show()
+
+
+# # Distribuição do Target Clases
+# sns.countplot(x='Target', data=df_ibovespa)
+# plt.title('Distribuição das Classes (Target)')
+# plt.xlabel('Tendência (0 = Queda, 1 = Alta)')
+# plt.ylabel('Quantidade de Dias')
+# plt.tight_layout()
+# plt.show()
